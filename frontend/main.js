c4ccad3c1e647c6846b649a8374a7eacad9978a2
LeoITPro <leoitpro@outlook.com>
2022-04-13 20:44:52 -0400

    Deployed storage and function

--- frontend/main.js
+++ frontend/main.js
@@ -1,12 +1,12 @@
 window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
-
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://leogetresumecounter.azurewebsites.net/api/GetResumeCounter?code=E1o3dipaQ3kEP9npOoUmhPFVITSYUxVsD69PXkvr7M246GkanLYp4w=='
+const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.");
